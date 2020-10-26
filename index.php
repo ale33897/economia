59df7f02d955581fe8ab578f265ff7b12d9a5df0
Alex <ale@plusglobal.com>
2020-10-26 13:50:07 -0300

    mejoras

---
+++ index.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta property="og:title" content="Economía Política Cátedra B"/>
+
+        <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+        <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+        
+        <script type="text/javascript">
+            var urls = [
+               "https://docs.google.com/forms/d/e/1FAIpQLSc4hPcwOeJ3EXaihU211sJlCCZm8XQFB2UNf6jloGC_FE8Syg/viewform?usp=sf_link",
+               "https://docs.google.com/forms/d/e/1FAIpQLScIHwxvKKJMRfNPzUUVVmvq_rR8crb1X18yynhmMkRd5IjBaw/viewform?usp=sf_link",
+               "https://docs.google.com/forms/d/e/1FAIpQLSfybs_Jrp9e5QGJWczrgA13_wwQ6v5OLv18dw5lhNZLDrHdyw/viewform?usp=sf_link",
+               "https://docs.google.com/forms/d/e/1FAIpQLSfpN3snwOHjViKTXAogV-2UmPlM0aeC7ZhmCwuVaZQfz6TpfQ/viewform?usp=sf_link",
+               "https://docs.google.com/forms/d/e/1FAIpQLSeuajQQvT_vIeAJb5m_wMxFovnmqPtztJ258N1g1A8IXI0aSw/viewform?usp=sf_link",
+               "https://docs.google.com/forms/d/e/1FAIpQLSd_szmVcRi8dcb05EA5bu5CM0Pd5UX3ZgX9LksLDf7HlCeceQ/viewform?usp=sf_link",
+            ];
+
+            function goSomewhere() {
+                var url = urls[Math.floor(Math.random()*urls.length)];
+                window.location = url; // redirect
+            }
+           
+        </script> 
+        <?php /*<script type="text/javascript">
+            var key = 0;
+            var urls = [
+               "https://docs.google.com/forms/d/e/1FAIpQLSc4hPcwOeJ3EXaihU211sJlCCZm8XQFB2UNf6jloGC_FE8Syg/viewform?usp=sf_link",
+               "https://docs.google.com/forms/d/e/1FAIpQLScIHwxvKKJMRfNPzUUVVmvq_rR8crb1X18yynhmMkRd5IjBaw/viewform?usp=sf_link",
+               "https://docs.google.com/forms/d/e/1FAIpQLSfybs_Jrp9e5QGJWczrgA13_wwQ6v5OLv18dw5lhNZLDrHdyw/viewform?usp=sf_link",
+               "https://docs.google.com/forms/d/e/1FAIpQLSfpN3snwOHjViKTXAogV-2UmPlM0aeC7ZhmCwuVaZQfz6TpfQ/viewform?usp=sf_link",
+               "https://docs.google.com/forms/d/e/1FAIpQLSeuajQQvT_vIeAJb5m_wMxFovnmqPtztJ258N1g1A8IXI0aSw/viewform?usp=sf_link",
+               "https://docs.google.com/forms/d/e/1FAIpQLSd_szmVcRi8dcb05EA5bu5CM0Pd5UX3ZgX9LksLDf7HlCeceQ/viewform?usp=sf_link",
+            ];
+
+            function goSomewhere() {
+                var url = urls[key];
+                window.location = url; // redirect
+                if (key >= urls.length) {
+                    key = 0;
+                } else {
+                    key = Key + 1;
+                }
+            }
+           
+        </script>*/ ?>
+        <style>
+            img.logo {
+                width: 50%;
+                margin: auto;
+                text-align: center;
+                display: block;
+            }
+            body {
+                margin: 40px 0;
+                display: block;
+                background-color: #f3faff;
+            }
+            a.botom {
+                padding: 18px 25px;
+                background-color: #002B49;
+                color: #fff;
+                text-transform: uppercase; 
+                margin: 30px auto;
+                text-decoration:none;
+                font-family: arial;
+                display: block;
+                width: 180px;
+                text-align: center;
+                font-size: 20px;
+            }
+            a.botom:hover {
+                background-color: #003f6b;
+            }
+            h1 {
+                text-align: center;
+                color: #002B49;
+                font-family: Arial;
+                text-transform: uppercase; 
+            }
+            p {
+                font-family: Arial;
+                text-align: center;
+                color: #333;
+            }
+        </style>
+        
+        
+    </head>
+    <body>
+        <img class="logo" data-aos="fade-up" src="logo.png">
+        <div>
+            <h1>Bienvenides al examen de<br> Economía Política Cátedra B</h1>
+            <?php /*<a class="botom" data-aos="fade-up" data-aos-duration="300" href="#" onClick="goSomewhere(); return false;">Iniciar examen</a>*/ ?>
+            <a class="botom" data-aos="fade-up" data-aos-duration="300" href="#" onClick="goSomewhere(); return false;">Iniciar examen</a>
+        </div>
+        <script>
+          AOS.init();
+        </script>
+    </body>
+</html> 
+
+
